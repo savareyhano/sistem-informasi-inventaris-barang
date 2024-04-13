7b26ce037cd65dad70d6764985aa7ba96ee31c5d
savareyhano <savareyhano99@gmail.com>
2024-04-13 16:18:48 +0700

    chore: update server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 require("dotenv").config();
-const host = process.env.HOSTNAME || 'localhost';
-const port = process.env.PORT || 3000;
+const host = process.env.HOSTNAME;
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Listening to the server on http://${host}:${port}`);
